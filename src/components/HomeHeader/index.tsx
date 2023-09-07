49f07480609f1fdfe121bc1e0fef78690c9a7a47
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-07 12:12:17 -0300

    add logout in HomeHeader

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,21 +1,30 @@
 import { Container, Greeting, Message, Name, Picture } from "./styles";
 import { TouchableOpacity } from "react-native";
+import { useUser, useApp } from "@realm/react";
+
 import { Power } from "phosphor-react-native";
 import { theme } from "../../theme";
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
       <Picture
-        source={{ uri: "https://github.com/mateusrc-dev.png" }}
+        source={{ uri: user?.profile.pictureUrl }}
         placeholder="L184i9kCW=of00ayjZay~qj[ayj@"
       />
       <Greeting>
         <Message>Olá</Message>
-        <Name>Mateus Raimundo</Name>
+        <Name>{user?.profile.name}</Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
