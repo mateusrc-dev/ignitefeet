0def9a698d209d750ba3d358977d5d116f4ccb09
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-07 17:46:25 -0300

    add RealmProvider in app

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import { theme } from "./src/theme";
 
 import { Routes } from "./src/routes";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { RealmProvider } from "./src/libs/realm";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -34,7 +35,9 @@ export default function App() {
             translucent
           />
           <UserProvider fallback={SignIn}>
-            <Routes />
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
           </UserProvider>
         </SafeAreaProvider>
       </ThemeProvider>
