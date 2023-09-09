bab0ff1c81e3af1d54db74a9acda0c57ca4ec92c
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-08 23:02:12 -0300

    created component LicensePlaceInput

---
+++ src/components/LicensePlaceInput/index.tsx
@@ -0,0 +1,24 @@
+import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
+import { Container, Input, Label } from "./styles";
+
+type Props = TextInputProps & {
+  label: string;
+};
+
+export function LicensePlateInput({ label, ...rest }: Props) {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Label>{label}</Label>
+
+      <Input
+        {...rest}
+        maxLength={7}
+        autoCapitalize="characters"
+        placeholderTextColor={COLORS.GRAY_400}
+      />
+    </Container>
+  );
+}
