b9e7567e082296b1a70e5d6fdd42b8b7b84b9b98
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-09 00:52:05 -0300

    created ref of element TextInput

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,19 +1,39 @@
+import { TextInput } from "react-native";
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlaceInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
+import { useRef } from "react";
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("ok!");
+  }
+
   return (
     <Container>
       <Header title="Saída" />
       <Content>
-        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA1234"
+          onSubmitEditing={() => descriptionRef.current?.focus()}
+          returnKeyType="next"
+        />
 
         <TextAreaInput
+          ref={descriptionRef}
           label="Finalidade"
           placeholder="Vou utilizar o veículo para..."
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType="send"
+          blurOnSubmit
         />
+
+        <Button title="Registrar Saída" onPress={handleDepartureRegister} />
       </Content>
     </Container>
   );
