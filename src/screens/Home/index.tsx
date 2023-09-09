bc62ebe98af7b1c0ed4a70e47eac4d47815b4bfd
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-09 17:53:52 -0300

    fetch Historic of vehicle

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,20 +2,47 @@ import { useNavigation } from "@react-navigation/native";
 import { CarStatus } from "../../components/CarStatus";
 import { HomeHeader } from "../../components/HomeHeader";
 import { Container, Content } from "./styles";
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
   const { navigate } = useNavigation();
+
+  const historic = useQuery(Historic);
 
   function handleRegisterMovement() {
     navigate("departure");
   }
+
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert(
+        "Veículo em uso",
+        "Não foi possível carregar o veículo em uso."
+      );
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus licensePlate={"XXX-0000"} onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   );
