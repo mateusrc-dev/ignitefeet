077607a3802e2547fe4da381765542154b82f714
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-09 18:29:57 -0300

    created screen Arrival

---
+++ src/screens/Arrival/index.tsx
@@ -0,0 +1,15 @@
+import { useRoute } from "@react-navigation/native";
+import { Container } from "./styles";
+
+type RouteParamsProps = {
+  id: string;
+};
+
+export function Arrival() {
+  const route = useRoute();
+  const { id } = route.params as RouteParamsProps;
+
+  console.log(id);
+
+  return <Container></Container>;
+}
