cd616d8bf8f6a5e12b04b8dc84fd15bae40d9a0f
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-10 10:57:28 -0300

    created component ButtonIcon and screen Arrival

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,16 @@
 import { useRoute } from "@react-navigation/native";
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from "./styles";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { X } from "phosphor-react-native";
 
 type RouteParamsProps = {
   id: string;
@@ -9,7 +20,26 @@ export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
 
-  console.log(id);
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
+        <LicensePlate>XXX0000</LicensePlate>
 
-  return <Container></Container>;
+        <Label>Finalidade</Label>
+        <Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          explicabo nulla sint quod, ducimus in voluptas, amet illum sequi
+          delectus atque excepturi deserunt sunt ea. Facere pariatur possimus
+          amet voluptatum!
+        </Description>
+
+        <Footer>
+          <ButtonIcon icon={X} />
+          <Button title="Registrar Chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
--- src/screens/Arrival/styles.ts
+++ src/screens/Arrival/styles.ts
@@ -1,6 +1,43 @@
 import styled from "styled-components/native";
+import { theme } from "../../theme";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: red;
-`
+    background-color: ${theme.COLORS.GRAY_800};
+`;
+
+export const Content = styled.View`
+    flex-grow: 1;
+    padding: 32px;
+`;
+
+export const Label = styled.Text`
+    color: ${theme.COLORS.GRAY_300};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+
+    margin-top: 32px;
+    margin-bottom: 5px;
+`;
+
+export const LicensePlate = styled.Text`
+    color: ${theme.COLORS.GRAY_100};
+    font-size: ${theme.FONT_SIZE.XXXL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+`;
+
+export const Description = styled.Text`
+    color: ${theme.COLORS.GRAY_100};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+
+    text-align: justify;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    gap: 16px;
+
+    margin-top: 32px;
+`;
