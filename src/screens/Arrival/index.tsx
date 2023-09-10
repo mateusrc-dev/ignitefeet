3a8bd26d6a109252c94aad74d09814d349e37b51
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-10 14:22:52 -0300

    created function for register arrival of vehicle

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -43,6 +43,28 @@ export function Arrival() {
     goBack();
   }
 
+  function handleArrivalRegister() {
+    try {
+      if (!historic) {
+        return Alert.alert(
+          "Error",
+          "Não foi possível obter os dados para registrar a chegada do veículo."
+        );
+      }
+
+      realm.write(() => {
+        historic.status = "arrival";
+        historic.updated_at = new Date();
+      });
+
+      Alert.alert("Chegada", "Chegada registrada com sucesso!");
+      goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Não foi possível registrar a chegada do veículo.");
+    }
+  }
+
   return (
     <Container>
       <Header title="Chegada" />
@@ -55,7 +77,7 @@ export function Arrival() {
 
         <Footer>
           <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
-          <Button title="Registrar Chegada" />
+          <Button title="Registrar Chegada" onPress={handleArrivalRegister} />
         </Footer>
       </Content>
     </Container>
