36587dddd3b228d96d08875dc67e70632f131678
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-09-16 00:01:59 -0300

    save in storage the locations of background task

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -19,6 +19,7 @@ import { Alert } from "react-native";
 import { useEffect, useState } from "react";
 import { getLastAsyncTimestamp } from "../../libs/asyncStorage/syncStorage";
 import { stopLocationTask } from "../../tasks/backgroundLocationTask";
+import { getStorageLocation } from "../../libs/asyncStorage/locationStorage";
 
 type RouteParamsProps = {
   id: string;
@@ -73,11 +74,19 @@ export function Arrival() {
     }
   }
 
+  async function getLocationsInfo() {
+    const lastSync = await getLastAsyncTimestamp();
+    const updatedAt = historic!.updated_at.getTime();
+
+    setDataNotSynced(updatedAt > lastSync);
+
+    const locationsStorage = await getStorageLocation();
+    console.log("STORAGE =>", locationsStorage);
+  }
+
   useEffect(() => {
-    getLastAsyncTimestamp().then((lastSync) =>
-      setDataNotSynced(historic!.updated_at.getTime() > lastSync)
-    );
-  }, []);
+    getLocationsInfo();
+  }, [historic]);
 
   return (
     <Container>
